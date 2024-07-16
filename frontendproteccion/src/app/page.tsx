19d1f42ba212d1bc32957c67c5be7806e46dd2e7
Mauricio Arias <imcrisam@gmail.com>
2024-07-16 10:28:56 -0500

    post and export static

--- frontendproteccion/src/app/page.tsx
+++ frontendproteccion/src/app/page.tsx
@@ -16,6 +16,14 @@ export default function Home() {
   const [visible, setVisible] = useState(false);
   const [isHistorial, setIsHistorial] = useState(false);
 
+  const [responseMessage, setResponseMessage] = useState("");
+
+  /**
+   * Calculates the Fibonacci sequence based on the given data string.
+   *
+   * @param {string} data - The data string in the format "HH:MM:SS".
+   * @return {void} This function does not return anything.
+   */
   function handleCalculateFibonaci(data: string) {
     const parts = data.split(":");
     const minutes = parts[1];
@@ -30,7 +38,33 @@ export default function Home() {
     setResults(fibo);
     context?.addDataItem({ key: data, values: fibo });
     setVisible(true);
+    postData(numberInit1, numberInit2, count);
   }
+
+  const postData = async (arg1: number, arg2: number, arg3: number) => {
+    try {
+      const response = await fetch("http://localhost:5000/fibo/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // Aquí puedes agregar otros headers según sea necesario
+        },
+        body: JSON.stringify({ arg1, arg2, arg3 }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Error en la solicitud POST");
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+      setResponseMessage(data.message); // Actualiza el estado con la respuesta del servicio
+    } catch (error) {
+      console.error("Error al enviar la solicitud POST:", error);
+      // Aquí puedes manejar el error de manera adecuada (mostrar un mensaje, etc.)
+    }
+  };
 
   return (
     <Card className="" style={{ textAlign: "center", margin: "0 auto" }}>
--- frontendproteccion/src/app/utils/fibonacci.ts
+++ frontendproteccion/src/app/utils/fibonacci.ts
@@ -1,9 +1,21 @@
+/**
+ * Generates a reverse Fibonacci sequence.
+ *
+ * @param {number} init1 - The first number in the sequence.
+ * @param {number} init2 - The second number in the sequence.
+ * @param {number} count - The number of elements in the sequence.
+ * @return {number[]} The reverse Fibonacci sequence.
+ */
 export function reverseFibonacci(
   init1: number,
   init2: number,
   count: number
 ): number[] {
   let sequence = [init1, init2];
+
+  if (init1 + init2 == 0) {
+    return sequence;
+  }
 
   for (let i = 2; i < count + 2; i++) {
     let next = sequence[i - 1] + sequence[i - 2];
